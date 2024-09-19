7d5aaf58ebe91849f41348b002cfb15329393cfe
Sara Achieng <achieng997@gmail.com>
2024-09-19 08:50:46 +0300

    Generating migrations

---
+++ app/migrations/versions/63c49f743b46_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 63c49f743b46
+Revises: 
+Create Date: 2024-09-19 08:26:44.530264
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63c49f743b46'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
